e480c9fce9f6b40a12c9626cff7eace1a1538ce2
Rahmat R <rahmat.rama98@gmail.com>
2020-08-18 09:26:11 +0700

    Service-worker init modif

--- src/script/service-worker/init.js
+++ src/script/service-worker/init.js
@@ -1,5 +1,5 @@
 import {registerRoute} from 'workbox-routing';
-import {StaleWhileRevalidate, CacheFirst} from 'workbox-strategies';
+import {StaleWhileRevalidate, CacheFirst, CacheOnly} from 'workbox-strategies';
 import {CacheableResponsePlugin} from 'workbox-cacheable-response';
 import {ExpirationPlugin} from 'workbox-expiration';
 import {precacheAndRoute} from 'workbox-precaching';
@@ -18,10 +18,11 @@ precacheAndRoute(urlsToCache);
 
 registerRoute(
     new RegExp('/pages/'),
-    new StaleWhileRevalidate()
+    new StaleWhileRevalidate({
+        cacheName: 'pages'
+    })
 );
 
-// Menyimpan cache dari CSS Google Fonts
 registerRoute(
     /^https:\/\/fonts\.googleapis\.com/,
     new StaleWhileRevalidate({
@@ -29,7 +30,6 @@ registerRoute(
     })
 );
 
-// Menyimpan cache untuk file font selama 1 tahun
 registerRoute(
     /^https:\/\/fonts\.gstatic\.com/,
     new CacheFirst({
@@ -59,68 +59,7 @@ registerRoute(
     }),
 );
 
-// console.log(urlsToCache);
-//
-// self.addEventListener("install", function (event) {
-//     self.skipWaiting();
-//     event.waitUntil(
-//         caches.open(CACHE_NAME).then(function(cache) {
-//             return cache.addAll(urlsToCache);
-//         })
-//     );
-// });
-//
-// self.addEventListener("activate", function (event) {
-//     event.waitUntil(
-//         caches.keys().then(function(cacheNames) {
-//             return Promise.all(
-//                 cacheNames.map(function(cacheName) {
-//                     if (cacheName !== CACHE_NAME) {
-//                         console.log("ServiceWorker: cache " + cacheName + " dihapus");
-//                         return caches.delete(cacheName);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-//
-// self.addEventListener("fetch", function(event) {
-//     const base_url = "https://api.football-data.org/v2/";
-//     if (event.request.url.indexOf(base_url) > -1) {
-//         event.respondWith(
-//             caches.open(CACHE_NAME).then(function(cache) {
-//                 return fetch(event.request).then(function(response) {
-//                     cache.put(event.request.url, response.clone());
-//                     return response;
-//                 })
-//             })
-//         );
-//     } else {
-//         event.respondWith(
-//             caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
-//                 return response || fetch (event.request);
-//             })
-//         )
-//     }
-// });
-//
-// self.addEventListener('push', function(event) {
-//     let body;
-//     if (event.data) {
-//         body = event.data.text();
-//     } else {
-//         body = 'Push message no payload';
-//     }
-//     const options = {
-//         body: body,
-//         icon: 'icon/icon-48.png',
-//         vibrate: [100, 50, 100],
-//         data: {
-//             dateOfArrival: Date.now(),
-//             primaryKey: 1
-//         }
-//     };
-//
-//     event.waitUntil(self.registration.showNotification('Push Notification', options));
-// });
+registerRoute(
+    new RegExp('/v2/'),
+    new CacheOnly()
+);
