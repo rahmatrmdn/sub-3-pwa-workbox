d01e051e4f23760cfc69a44bc45f0bbe6f3e705c
Rahmat R <rahmat.rama98@gmail.com>
2020-08-18 09:45:44 +0700

    submission 3 done!

--- src/script/service-worker/init.js
+++ src/script/service-worker/init.js
@@ -60,6 +60,26 @@ registerRoute(
 );
 
 registerRoute(
-    new RegExp('/v2/'),
-    new CacheOnly()
+    new RegExp('https://api.football-data.org/v2/'),
+    new StaleWhileRevalidate()
 );
+
+self.addEventListener('push', function(event) {
+    let body;
+    if (event.data) {
+        body = event.data.text();
+    } else {
+        body = 'Push message no payload';
+    }
+    const options = {
+        body: body,
+        icon: 'icon/icon-48.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+        }
+    };
+
+    event.waitUntil(self.registration.showNotification('Push Notification', options));
+});
