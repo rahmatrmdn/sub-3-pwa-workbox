bdc8d5a40fadd7cae3fb9458d324afe83f875769
Rahmat R <rahmat.rama98@gmail.com>
2020-08-18 08:42:39 +0700

    start with workbox

--- src/script/service-worker/init.js
+++ src/script/service-worker/init.js
@@ -1,83 +1,113 @@
-const DEV = true
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
+
+if (workbox)
+    console.log(`Workbox berhasil dimuat`);
+else
+    console.log(`Workbox gagal dimuat`);
 
 const { assets } = global.serviceWorkerOption
-let CACHE_NAME
 
-if (DEV) {
-    CACHE_NAME = new Date().toISOString()
-} else {
-    CACHE_NAME = "football-apps-v10"
-}
-
-let urlsToCache = [...assets,
-    './',
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v54/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2']
-
-urlsToCache = urlsToCache.map(path => {
-    return new URL(path, global.location).toString()
+const urlsToCache = assets.map(path => {
+    return {
+        url: path,
+        revision: '1'
+    }
 })
 
-self.addEventListener("install", function (event) {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
+workbox.precaching.precacheAndRoute(urlsToCache);
 
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate()
+);
 
-self.addEventListener("fetch", function(event) {
-    const base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function(cache) {
-                return fetch(event.request).then(function(response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
-                return response || fetch (event.request);
-            })
-        )
-    }
-});
+// Menyimpan cache dari CSS Google Fonts
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts-stylesheets',
+    })
+);
 
-self.addEventListener('push', function(event) {
-    let body;
-    if (event.data) {
-        body = event.data.text();
-    } else {
-        body = 'Push message no payload';
-    }
-    const options = {
-        body: body,
-        icon: 'icon/icon-48.png',
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: 1
-        }
-    };
+// Menyimpan cache untuk file font selama 1 tahun
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'google-fonts-webfonts',
+        plugins: [
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200],
+            }),
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30,
+            }),
+        ],
+    })
+);
 
-    event.waitUntil(self.registration.showNotification('Push Notification', options));
-});
+// console.log(urlsToCache);
+//
+// self.addEventListener("install", function (event) {
+//     self.skipWaiting();
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then(function(cache) {
+//             return cache.addAll(urlsToCache);
+//         })
+//     );
+// });
+//
+// self.addEventListener("activate", function (event) {
+//     event.waitUntil(
+//         caches.keys().then(function(cacheNames) {
+//             return Promise.all(
+//                 cacheNames.map(function(cacheName) {
+//                     if (cacheName !== CACHE_NAME) {
+//                         console.log("ServiceWorker: cache " + cacheName + " dihapus");
+//                         return caches.delete(cacheName);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+// });
+//
+// self.addEventListener("fetch", function(event) {
+//     const base_url = "https://api.football-data.org/v2/";
+//     if (event.request.url.indexOf(base_url) > -1) {
+//         event.respondWith(
+//             caches.open(CACHE_NAME).then(function(cache) {
+//                 return fetch(event.request).then(function(response) {
+//                     cache.put(event.request.url, response.clone());
+//                     return response;
+//                 })
+//             })
+//         );
+//     } else {
+//         event.respondWith(
+//             caches.match(event.request, {'ignoreSearch': true}).then(function(response) {
+//                 return response || fetch (event.request);
+//             })
+//         )
+//     }
+// });
+//
+// self.addEventListener('push', function(event) {
+//     let body;
+//     if (event.data) {
+//         body = event.data.text();
+//     } else {
+//         body = 'Push message no payload';
+//     }
+//     const options = {
+//         body: body,
+//         icon: 'icon/icon-48.png',
+//         vibrate: [100, 50, 100],
+//         data: {
+//             dateOfArrival: Date.now(),
+//             primaryKey: 1
+//         }
+//     };
+//
+//     event.waitUntil(self.registration.showNotification('Push Notification', options));
+// });
